fe6d846e3cd963dcf1901361911559be1cb3f9c9
Martin Hettiger <martin@hettiger.com>
2020-05-30 14:01:42 +0200

    Initial Commit

---
+++ LeakingAccessoryDetailButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeakingAccessoryDetailButton
+//
+//  Created by Martin Hettiger on 30.05.20.
+//  Copyright © 2020 Martin Hettiger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
